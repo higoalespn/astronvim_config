192ce9d171f303a6e1ac71155b95c69daf0b9d2e
higoalespn <34319212@qq.com>
2024-09-03 17:01:22 +0800

    change: change the timeout and max width for notify

--- lua/plugins/nvim-notify.lua
+++ lua/plugins/nvim-notify.lua
@@ -3,9 +3,9 @@ return {
   opts = {
     stages = "static",
     render = "compact",
-    max_width = "30",
+    max_width = "120",
     fps = 5,
     level = 1,
-    timeout = 1000,
+    timeout = 3000,
   },
 }
